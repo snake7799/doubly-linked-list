f67eec58db55fc8fa1139f805e1b44e70e0923be
Pavel Egorov <snake7799@users.noreply.github.com>
2017-01-28 17:53:06 +0300

    Add files via upload
    Implemented all class methods.

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,152 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.length = 0;
+        this._head = new Node();
+        this._tail = new Node(); 
 
-    append(data) {}
+    }
 
-    head() {}
+    append(data) {
+        let newNode = new Node(data);
 
-    tail() {}
+        if (this.isEmpty())
+            this._head = this._tail = newNode;
+        else {
+            newNode.prev = this._tail;
+            this._tail.next = this._tail = newNode;
+        }
 
-    at(index) {}
+        this.length++;
 
-    insertAt(index, data) {}
+        return this;
+    }
 
-    isEmpty() {}
+    head() {
+        if (this._head != null)
+            return this._head.data;
+        else
+            return null;
+    }
 
-    clear() {}
+    tail() {
+        if (this._head != null)
+            return this._tail.data;
+        else 
+            return null;
+    }
 
-    deleteAt(index) {}
+    at(index) {
+        if (index >= 0 && index < this.length) {
+            let needNode = this._head;
 
-    reverse() {}
+            for (let i = 0; i < index; i++)
+                needNode = needNode.next;
 
-    indexOf(data) {}
+            return needNode.data;
+        }
+        else
+            return -1;
+    }
+
+    insertAt(index, data) { 
+        if (index >= 0 && index < this.length) {
+            let newNode = new Node(data),
+                tempNode = new Node();
+
+            tempNode.next = this._head;
+
+            for (let i = 0; i < index; i++)
+                tempNode = tempNode.next;
+
+            newNode.next = tempNode.next = newNode;
+
+            this.length++;
+
+            return this;
+        }
+        else
+            return -1;
+    }
+
+    isEmpty() {
+        if (this.length == 0)
+            return true;
+        else
+            return false;
+    }
+
+    clear() {
+        this._head = this._tail = null;
+        this.length = 0;
+
+        return this;
+    }
+
+    deleteAt(index) {
+        if (index >= 0 && index < this.length) {
+            let needNode = this._head;
+
+            if (index == 0) {
+                this._head = needNode.next;
+
+                if (this._head)
+                    this._head.prev = null;
+                else
+                    this._tail = null;
+            } 
+            else if (index == this.length - 1) {
+                needNode = this._tail;
+                this._tail = needNode.prev;
+                this._tail.next = null;
+            } 
+            else {                               
+                for (let i = 0; i < index; i++)
+                    needNode = needNode.next;
+
+                needNode.prev.next = needNode.next;
+            }
+
+            this.length--; 
+
+            return this;
+        } 
+        else 
+            return -1;
+    }
+
+    reverse() { 
+        let node = new Node(),
+            temp = new Node(); 
+
+        this._tail = node = this._head;
+
+        while (node) {
+            temp = node.prev;
+            node.prev = node.next;
+            node.next = temp;
+            node = node.prev;
+        }
+
+        if (temp)
+            this._head = temp.prev;
+
+        return this;
+    }
+
+    indexOf(data) {
+        let searchNode = this._head;
+
+        for (let i = 0; i < this.length; i++) {
+            if (searchNode.data == data)
+                return i;
+            else if (i == this.length - 1)
+                return -1;
+
+            searchNode = searchNode.next;
+        }
+    }
 }
 
 module.exports = LinkedList;
